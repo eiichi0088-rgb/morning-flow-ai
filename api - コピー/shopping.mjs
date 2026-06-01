3627894d46300f9aa634373a6b6ae4de9f905e86
eiichi0088-rgb <eiichi0088@gmail.com>
2026-06-01 13:35:43 +0900

    Prepare direct-edit workflow

--- api - コピー/shopping.mjs
+++
@@ -1,5 +0,0 @@
-import { handleShoppingRequest } from '../scripts/openai-shopping-handler.mjs';
-
-export default async function handler(request, response) {
-  await handleShoppingRequest(request, response);
-}
